48e15d9357c58215ba3362ac4ff70afb6063dcfe
Dawid Boksa <boksa.dawid@interia.pl>
2018-04-06 01:33:43 +0200

    added some comments, user input for word

--- monkeywritingmachine.rb
+++ monkeywritingmachine.rb
@@ -1,9 +1,11 @@
 def monkeywritingmachine
   letters = ('a'..'z').to_a
+  #positions in array for randomized letters 
   aone = rand(27)
   bone = rand(27)
   cone = rand(27)
   done = rand(27)
+  #letters of randomized word
 first = letters[aone]
 second = letters[bone]
 third = letters[cone]
@@ -20,4 +22,6 @@ end
 puts "Monkey used writing machine #{number_of_times} times, to write #{@word}"
 end
 
-testingmachine("nati")
+puts "what monkeys must write?"
+your_word = gets.chomp
+testingmachine(your_word)
