32ce2c260ac636cf80a58c5355a63b7321301c9a
Dawid Boksa <boksa.dawid@interia.pl>
2018-04-27 20:10:45 +0200

    improved monkeywritingmachine

--- lib/monkeywritingmachine.rb
+++ lib/monkeywritingmachine.rb
@@ -1,18 +1,22 @@
-@word = ''
+
 def monkeywritingmachine
     @word = @word + ('a'..'z').to_a.shuffle[0]
 end
 
 def testingmachine(your_word)
-number_of_times = 0
-@your_word = your_word
-if @word != your_word.downcase
-  number_of_times += 1
- monkeywritingmachine
-end
-puts "Monkey used writing machine #{number_of_times} times, to write #{@word}"
+  number_of_times = 0
+  @word = ''
+  until @word == your_word.downcase
+    number_of_times += 1
+       if @word.length >= your_word.length
+         @word = '' #reseting the word
+       end
+  monkeywritingmachine #adding another letter
+  end
+number_of_times /= your_word.length
+return "Monkey used writing machine #{number_of_times} times, to write #{@word}"
 end
 
 puts "what monkeys must write?"
 word = gets.chomp
-testingmachine(word)
+puts testingmachine(word)
